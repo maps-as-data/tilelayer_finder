1327f41514ba05fa7ed686cf44a98c93acc5a61d
Rosie Wood <rwood@turing.ac.uk>
2023-10-09 09:21:50 +0100

    update run.py

--- src/tileserver_finder/run.py
+++ src/tileserver_finder/run.py
@@ -1,16 +1,24 @@
-import tileserver_finder
 from argparse import ArgumentParser
 
-parser = ArgumentParser()
-parser.add_argument("-c","--clean", action="store_true", help="Whether to clean data")
-parser.add_argument("-o","--output", nargs="?", type=str, default="./NLS_tilelayers.csv", const="./NLS_tilelayers.csv", help="Name to use when saving output file")
-parser.add_argument("-n","--name", nargs="?", type=str, default="oneinch2nd", const="oneinch2nd", help="Name of tilelayer to create metadata for")
-args = parser.parse_args()
+from tileserver_finder import finder
 
-# run the code 
-TSF = tileserver_finder.TileServerFinder()
-TSF.get_data(clean=args.clean)
-TSF.save_data(fname=args.output)
+def main():
+    # parse arguments
+    parser = ArgumentParser()
+    parser.add_argument("-c","--clean", action="store_true", help="Whether to clean data")
+    parser.add_argument("-o","--output", type=str, default="nls_tilelayers.csv", help="Name to use when saving output file")
+    parser.add_argument("-n","--name", nargs="*", type=str, default=[], help="Name of tilelayer to create metadata for")
+    args = parser.parse_args()
 
-# if you wanted to create a json of one of your tilelayers, uncomment the below and pass the "-n" argument to set which tilelayer to create metadata for
-# TSF.create_metadata_json(name=args.name)
+    # run tileserver_finder
+    tsf = finder.TileServerFinder()
+    tsf.get_data(clean=args.clean)
+    tsf.save_data(fname=args.output)
+
+    # save metadata if any names are passed
+    if len(args.name)>0:
+        for name in args.name:
+            tsf.create_metadata_json(name=name)
+
+if __name__ == "__main__":
+    main()
