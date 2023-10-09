9137b7354455c06164fe89577ea1a3539f5dc48a
Rosie Wood <rwood@turing.ac.uk>
2023-10-09 09:52:57 +0100

    run precommit

--- src/tileserver_finder/finder.py
+++ src/tileserver_finder/finder.py
@@ -1,18 +1,19 @@
-import requests
 import re
+
 import pandas as pd
+import requests
 from owslib.wfs import WebFeatureService
-from typing import Optional
+
 
 class TileServerFinder:
     """For finding tileservers available from the NLS."""
-    
+
     def __init__(self):
         """For finding tileservers available from the NLS."""
         self.get_wfs()
         self.queries = {}
 
-    def get_data(self, clean: Optional[bool] = True):
+    def get_data(self, clean: bool | None = True):
         """Gets the data and saves to `self.data`.
 
         Parameters
@@ -26,54 +27,72 @@ class TileServerFinder:
         self._extract_data(clean=clean)
 
     def _find_current_file(self):
-        """Find the url of the current live .js file and save to `self.current_file`.
-        """
-        with requests.get('https://maps.nls.uk/geo/version') as response:
-            current_file = re.findall(r"(?<=Current live file:\s<a\shref=\")(.*)(?=\")", response.text)[0]
+        """Find the url of the current live .js file and save to `self.current_file`."""
+        with requests.get("https://maps.nls.uk/geo/version") as response:
+            current_file = re.findall(
+                r"(?<=Current live file:\s<a\shref=\")(.*)(?=\")", response.text
+            )[0]
             print(f"[INFO] Current file is: '{current_file}'")
             self.current_file = current_file
-            
+
     def _generate_layers_dict(self):
-        """Generate a dictionary containing available tilelayers and save to `self.layers_dict`. 
+        """Generate a dictionary containing available tilelayers and save to `self.layers_dict`.
         Keys are tilelayer names.
         """
         current_file = self.current_file
-        
+
         with requests.get(current_file) as response:
             layers = re.findall(r"(?<=overlayLayers =  \[)(.*)(?=\])", response.text)[0]
             layers_list = re.split(r",\s*", layers)
-            all_layers_dict = {layer:re.findall(fr"(?<=var\s{layer}\b)\s+=\s+(.*?)(?=\);)", response.text, re.DOTALL)[0] for layer in layers_list}
+            all_layers_dict = {
+                layer: re.findall(
+                    rf"(?<=var\s{layer}\b)\s+=\s+(.*?)(?=\);)", response.text, re.DOTALL
+                )[0]
+                for layer in layers_list
+            }
 
-        tile_layers_dict = {k:v for k, v in all_layers_dict.items() 
-                            if re.search(r"new\s*ol.layer.Tile\(", v)}
-        
+        tile_layers_dict = {
+            k: v
+            for k, v in all_layers_dict.items()
+            if re.search(r"new\s*ol.layer.Tile\(", v)
+        }
+
         for k, v in tile_layers_dict.items():
-            tile_layers_dict[k]=re.sub(r"\n//.*", "", v)
-            
+            tile_layers_dict[k] = re.sub(r"\n//.*", "", v)
+
         self.layers_dict = tile_layers_dict
-        
-    def _extract_data(self, clean: Optional[bool] = True):
-        """Extract data (name, title, typename, XYZ url and maxZ) from `self.layers_dict` and save to `self.data`. 
+
+    def _extract_data(self, clean: bool | None = True):
+        """Extract data (name, title, typename, XYZ url and maxZ) from `self.layers_dict` and save to `self.data`.
 
         Parameters
         -----------
         clean : bool, optional
             Whether to clean up dataframe by removing duplicate and unavailable layers.
         """
-        data_dict={}
+        data_dict = {}
         for k, v in self.layers_dict.items():
             title = re.findall(r"(?<=title:)\s*[\"|\'](.*)(?=[\"|\'],)", v)
-            sourceXYZ = re.findall(r"(?<=source:)\s*new\sol.source.XYZ\(\{(.*)(?=\}\),)", v, re.DOTALL)
-            if len(sourceXYZ)>0:
-                XYZ = re.findall(r"(?<=url:)\s*[\"|\'](.*[png|jpg])(?=[\"|\'],)", sourceXYZ[0])
-                maxZ = re.findall(r"(?<=maxZ:)\s*(\d+)", sourceXYZ[0])
-                if len(maxZ)==0:
-                    maxZ = re.findall(r"(?<=maxZ:)\s*(\d+)", v)
+            source_xyz = re.findall(
+                r"(?<=source:)\s*new\sol.source.XYZ\(\{(.*)(?=\}\),)", v, re.DOTALL
+            )
+            if len(source_xyz) > 0:
+                xyz = re.findall(
+                    r"(?<=url:)\s*[\"|\'](.*[png|jpg])(?=[\"|\'],)", source_xyz[0]
+                )
+                max_z = re.findall(r"(?<=maxZ:)\s*(\d+)", source_xyz[0])
+                if len(max_z) == 0:
+                    max_z = re.findall(r"(?<=maxZ:)\s*(\d+)", v)
                 typename = re.findall(r"(?<=typename:)\s*[\"|\'](.*)(?=[\"|\'],)", v)
-                data_dict[k] = [value[0] if len(value)!=0 else None for value in [title, typename, XYZ, maxZ]]
-                
-        data = pd.DataFrame.from_dict(data_dict, orient="index", columns=["Title", "Typename", "XYZ URL", "Max Z"])
-        
+                data_dict[k] = [
+                    value[0] if len(value) != 0 else None
+                    for value in [title, typename, xyz, max_z]
+                ]
+
+        data = pd.DataFrame.from_dict(
+            data_dict, orient="index", columns=["Title", "Typename", "XYZ URL", "Max Z"]
+        )
+
         if clean:
             # remove 'nls:WFS' as these tend to be single maps rather than layers
             data = data[data["Typename"] != "nls:WFS"]
@@ -81,12 +100,12 @@ class TileServerFinder:
             for i, typename in data["Typename"].items():
                 if typename not in list(self.wfs.contents):
                     data.drop(i, inplace=True)
-        
+
         data.reset_index(inplace=True, names="Name")
         print(f"[INFO] Dataframe has {len(data)} values.")
         self.data = data
 
-    def save_data(self, fname : Optional[str] = "./NLS_tilelayers.csv"):
+    def save_data(self, fname: str | None = "./NLS_tilelayers.csv"):
         """Save extracted data (`self.data`) to csv file.
 
         Parameters
@@ -102,36 +121,35 @@ class TileServerFinder:
         self.data.to_csv(fname)
 
     def load_data(
-            self, 
-            file_path: str,
-            ):
+        self,
+        file_path: str,
+    ):
         """Loads a csv file containing the data (name, title, typename, XYZ url and maxZ).
 
         Parameters
         ----------
         file_path : str
             The path to file to load.
-        
+
         Notes
         -----
         The NLS periodically updates the .js file used to generate this data.
         If your data is over a few months old, it may be worth re-creating your csv file using the ``.get_data()`` method.
         """
-        
+
         data = pd.read_csv(file_path, index_col=0)
         self.data = data
 
     def list_tilelayers(self):
-        """Print the names and titles of available tilelayers.
-        """
+        """Print the names and titles of available tilelayers."""
         tilelayers = list(self.data["Name"])
         print(*tilelayers, sep="\n")
 
     def create_metadata_json(
         self,
         name: str,
-        bbox: Optional[tuple] = None,
-        srsname: Optional[str] = "urn:x-ogc:def:crs:EPSG:4326",
+        bbox: tuple | None = None,
+        srsname: str | None = "urn:x-ogc:def:crs:EPSG:4326",
     ):
         """Create a .json file containing the metadata for the named tilelayer.
 
@@ -144,45 +162,48 @@ class TileServerFinder:
             If None, the bounding box will be looked up for the chosen tilelayer.
             By default, None.
         srsname : Optional[str], optional
-            The spatial reference system (EPSG code) to request the data in. 
+            The spatial reference system (EPSG code) to request the data in.
             By default, "urn:x-ogc:def:crs:EPSG:4326".
         """
         if name in self.data["Name"]:
-            typename = self.data[self.data["Name"]==name]['Typename'].item()
+            typename = self.data[self.data["Name"] == name]["Typename"].item()
             print(f"[INFO] Getting metadata for {name} (typename: '{typename}')")
-            XYZ = self.data[self.data["Name"]==name]['XYZ URL'].item()
-            print(f"[INFO] XYZ URL for this layer is: '{XYZ}'")
-            maxZ = self.data[self.data["Name"]==name]['Max Z'].item()
-            print(f"[INFO] Max zoom level for this layer is: {maxZ}")
+            xyz = self.data[self.data["Name"] == name]["XYZ URL"].item()
+            print(f"[INFO] XYZ URL for this layer is: '{xyz}'")
+            max_z = self.data[self.data["Name"] == name]["Max Z"].item()
+            print(f"[INFO] Max zoom level for this layer is: {max_z}")
         else:
-            raise ValueError(f'"{name}" not found.')
+            msg = f'"{name}" not found.'
+            raise ValueError(msg)
 
         try:
             self.wfs.contents[typename]
-        except KeyError:
-            raise KeyError("[ERROR] The metadata for this tilelayer is unavailable.")
-        
+        except KeyError as err:
+            msg = "[ERROR] The metadata for this tilelayer is unavailable."
+            raise KeyError(msg) from err
+
         if not bbox:
             try:
-                bbox_wgs84 = self.wfs.contents[typename].boundingBoxWGS84 + ("EPSG:4326",)
+                bbox_wgs84 = (
+                    *self.wfs.contents[typename].boundingBoxWGS84,
+                    "EPSG:4326",
+                )
             except AttributeError:
-                (-9.26,49.77,2.73,60.97, "EPSG:4326")
+                bbox_wgs84 = (-9.26, 49.77, 2.73, 60.97, "EPSG:4326")
         wfs_feature = self.wfs.getfeature(
-            typename=typename,
-            bbox=bbox_wgs84,
-            outputFormat="json",
-            srsname=srsname)
-        
+            typename=typename, bbox=bbox_wgs84, outputFormat="json", srsname=srsname
+        )
+
         self.queries[name] = wfs_feature
 
         print(f"[INFO] Writing to file: './{name}.json'")
         with open(f"{name}.json", "wb") as f:
             f.write(wfs_feature.getbuffer())
-        
+
     def get_wfs(
-        self, 
-        url: Optional[str] = 'https://geoserver.nls.uk/geoserver/wfs', 
-        version: Optional[str] ='1.1.0'
+        self,
+        url: str | None = "https://geoserver.nls.uk/geoserver/wfs",
+        version: str | None = "1.1.0",
     ):
         """Get the WFS and save to `self.wfs`.
 
@@ -195,14 +216,14 @@ class TileServerFinder:
         """
         wfs = WebFeatureService(url, version)
         self.wfs = wfs
-        
+
     def print_found_queries(self):
         """Print the names of previously queried tilelayers.
         Each of these can be re-accessed using `self.queries["tilelayer_name"]`.
         """
         if len(self.queries) == 0:
             print("[INFO] No queries made so far.")
-            
+
         else:
             queried_wfs_features = list(self.queries.keys())
             print("[INFO] Queried WFS features:")
--- src/tileserver_finder/run.py
+++ src/tileserver_finder/run.py
@@ -2,12 +2,28 @@ from argparse import ArgumentParser
 
 from tileserver_finder import finder
 
+
 def main():
     # parse arguments
     parser = ArgumentParser()
-    parser.add_argument("-c","--clean", action="store_true", help="Whether to clean data")
-    parser.add_argument("-o","--output", type=str, default="nls_tilelayers.csv", help="Name to use when saving output file")
-    parser.add_argument("-n","--name", nargs="*", type=str, default=[], help="Name of tilelayer to create metadata for")
+    parser.add_argument(
+        "-c", "--clean", action="store_true", help="Whether to clean data"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="nls_tilelayers.csv",
+        help="Name to use when saving output file",
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        nargs="*",
+        type=str,
+        default=[],
+        help="Name of tilelayer to create metadata for",
+    )
     args = parser.parse_args()
 
     # run tileserver_finder
@@ -16,9 +32,10 @@ def main():
     tsf.save_data(fname=args.output)
 
     # save metadata if any names are passed
-    if len(args.name)>0:
+    if len(args.name) > 0:
         for name in args.name:
             tsf.create_metadata_json(name=name)
+
 
 if __name__ == "__main__":
     main()
